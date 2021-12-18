1bd7e0bf4d98f7f1b66b39521808197ce2d7fe5d
AliceOfSNU <khjune29@gmail.com>
2021-12-18 20:46:37 +0900

    private chat added

--- Assets/Scripts/TextChat/PhotonChatManager.cs
+++ Assets/Scripts/TextChat/PhotonChatManager.cs
@@ -28,13 +28,14 @@ namespace PhotonTextChat
         //default channels: automatically join on Connect()
         public string[] ChannelsToJoinOnConnect;
         public int HistoryLengthToFetch; // # previously sent messages that can be fetched for context
+
+        //username to use
         public string UserName;
 
         
         [Header("Panels")]
         //the ui elements - panels -> set in inspector
         public RectTransform ChatPanel;    
-        public GameObject UsernamePanel;
 
         [Header("UI")]
         //the ui elements - buttons and texts -> set in inspector
@@ -42,6 +43,9 @@ namespace PhotonTextChat
         public InputField InputFieldSendTo;
         public Button SendButton;
         public Text CurrentChannelText;
+
+        //This is the Dropdown
+        public Dropdown ChannelDropdown;
 
         //set in inspector!
         [SerializeField] 
@@ -52,17 +56,17 @@ namespace PhotonTextChat
         void Start()
         {
             DontDestroyOnLoad(this.gameObject);
-            UsernamePanel.SetActive(false);
             if (string.IsNullOrEmpty(this.UserName))
             {
-                //made-up username
-                this.UserName = "default user";
+                //assign the name set in editor to the client.
+                UserName = "default user";
             }
 
             //add listeners to input field and send button
             InputFieldChat.onEndEdit.AddListener(delegate { OnEnterSend(); });
             SendButton.onClick.AddListener(delegate { OnClickSend(); });
-        
+            ChannelDropdown.ClearOptions();
+            ChannelDropdown.onValueChanged.AddListener(delegate { OnSwitchChannel(); });
             //connect on start
             Connect();
         }
@@ -74,7 +78,7 @@ namespace PhotonTextChat
             this.chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat,
                 PhotonNetwork.AppVersion, new AuthenticationValues(userID));
 
-            Debug.Log("Connecting as: " + this.UserName);
+            Debug.Log("Connecting as: " + UserName);
         }
 
 
@@ -95,18 +99,20 @@ namespace PhotonTextChat
             if (Input.GetKey(KeyCode.Return) || Input.GetKey(KeyCode.KeypadEnter))
             {
                 Debug.Log("Enter clicked");
-                this.SendChatMessage(this.InputFieldChat.text, this.InputFieldSendTo.text);
-                this.InputFieldChat.text = "";
+                SendChatMessage(InputFieldChat.text, InputFieldSendTo.text);
+                InputFieldChat.text = "";
+                InputFieldSendTo.text = "";
             }
         }
 
         //on send button clicked
         void OnClickSend()
         {
-            if (this.InputFieldChat != null)
+            if (InputFieldChat != null)
             {
-                this.SendChatMessage(this.InputFieldChat.text, this.InputFieldSendTo.text);
-                this.InputFieldChat.text = "";
+                SendChatMessage(InputFieldChat.text, InputFieldSendTo.text);
+                InputFieldChat.text = "";
+                InputFieldSendTo.text = "";
             }
         }
 
@@ -120,14 +126,11 @@ namespace PhotonTextChat
             }
 
             //is this message private?
-            bool doingPrivateChat = this.chatClient.PrivateChannels.ContainsKey(this.selectedChannelName);
+            bool doingPrivateChat = !string.IsNullOrEmpty(toUser);
             string privateChatTarget = string.Empty;
             if (doingPrivateChat)
             {
-                // the channel name for a private is in form:  user1 : user2
-                //we need the user2 part.
-                string[] splitNames = this.selectedChannelName.Split(new char[] { ':' });
-                privateChatTarget = splitNames[1];
+                privateChatTarget = toUser;
             }
 
             if (inputLine[0].Equals('\\')) //commands begin with character '\'
@@ -144,7 +147,7 @@ namespace PhotonTextChat
                 {
                     this.chatClient.Unsubscribe(tokens[1].Split(new char[] { ' ', ',' }));
                 }
-                //clearing channel - delete all messages (testing purpose)
+                //clearing channel - renove channel / for testing only
                 else if (tokens[0].Equals("\\clear"))
                 {
                     if (doingPrivateChat)
@@ -166,9 +169,9 @@ namespace PhotonTextChat
                     string[] subtokens = tokens[1].Split(new char[] { ' ', ',' }, 2);
 
                     //already subscribed to the channel :switch to it
-                    if (this.myChannels.Contains(subtokens[0]))
+                    if (myChannels.Contains(subtokens[0]))
                     {
-                        this.ShowChannel(subtokens[0]);
+                        ShowChannel(subtokens[0]);
                     }
                     else //not yet joined - create a new channel and enter
                     {
@@ -180,7 +183,7 @@ namespace PhotonTextChat
                     Debug.Log("The command '" + tokens[0] + "' is invalid.");
                 }
             }
-            else //messages
+            else //send messages
             {
                 if (doingPrivateChat)
                 {
@@ -214,6 +217,12 @@ namespace PhotonTextChat
             Debug.Log("disconnected");
         }
 
+        //switch to channel selected on dropdown
+        public void OnSwitchChannel()
+        {
+            ShowChannel(ChannelDropdown.options[ChannelDropdown.value].text);
+        }
+
         //this shows messages buffered on the channel
         public void ShowChannel(string channelName)
         {
@@ -228,28 +237,45 @@ namespace PhotonTextChat
             }
 
             //switch to this channel
-            this.selectedChannelName = channelName;
+            selectedChannelName = channelName;
+
+            //update selection (dropdown)
+            for (int i = 0; i < ChannelDropdown.options.Count; ++i)
+            {
+                if (ChannelDropdown.options[i].Equals(channelName)) ChannelDropdown.value = i;
+            } 
 
             //display message
-            this.CurrentChannelText.text = channel.ToStringMessages();
-            Debug.Log("ShowChannel: " + this.selectedChannelName);
+            CurrentChannelText.text = channel.ToStringMessages();
+            Debug.Log("ShowChannel: " + selectedChannelName);
 
         }
 
         //get message callbacks
         public void OnGetMessages(string channelName, string[] senders, object[] messages)
         {
-            if (channelName.Equals(this.selectedChannelName))
+            if (channelName.Equals(selectedChannelName))
             {
-                this.ShowChannel(this.selectedChannelName);
+                ShowChannel(selectedChannelName);
             }
         }
 
         public void OnPrivateMessage(string sender, object message, string channelName)
         {
+            //add to channels.
+            if (!myChannels.Contains(channelName))
+            {
+                myChannels.Add(channelName);
+
+                Dropdown.OptionData option = new Dropdown.OptionData();
+                option.text = channelName;
+                ChannelDropdown.options.Add(option);
+            }
+
+            
             if (channelName.Equals(this.selectedChannelName))
             {
-                this.ShowChannel(channelName);
+                ShowChannel(channelName);
             }
         }
 
@@ -258,42 +284,55 @@ namespace PhotonTextChat
         //subscription callbacks
         public void OnSubscribed(string[] channels, bool[] results)
         {
+            Dropdown.OptionData option;
             //notify entry on each channel
             foreach (string channel in channels)
             {
                 this.chatClient.PublishMessage(channel, "says 'hi'.");
                 //add to my channels
-                if(!myChannels.Contains(channel)) this.myChannels.Add(channel);
+                if (!myChannels.Contains(channel))
+                {
+                    myChannels.Add(channel);
+                    option = new Dropdown.OptionData();
+                    option.text = channel;
+                    ChannelDropdown.options.Add(option);
+                }
             }
             Debug.Log("OnSubscribed: " + string.Join(", ", channels));
             
             // Switch to the first newly joined channel
-            this.ShowChannel(channels[0]);
+            ShowChannel(channels[0]);
+            
         }
 
         public void OnUnsubscribed(string[] channels)
         {
             foreach (string channelName in channels)
             {
-                if (this.myChannels.Contains(channelName))//if i'm subscribed to the channel
+                if (myChannels.Contains(channelName))//if i'm subscribed to the channel
                 {
                     Debug.Log("Unsubscribed from channel '" + channelName + "'.");
 
                     // Showing another channel if the active channel is the one we unsubscribed from
-                    if (channelName == this.selectedChannelName && this.myChannels.Count > 0)
+                    if (channelName == selectedChannelName && myChannels.Count > 0)
                     {
                         //get enumerator(like cpp iterator) to the first in the channels.
-                        IEnumerator<string> firstEntry = this.myChannels.GetEnumerator();
+                        IEnumerator<string> firstEntry = myChannels.GetEnumerator();
                         firstEntry.MoveNext();
                         //IEnumerator.Current gets the element pointed to by the enumerator in the collection
-                        this.ShowChannel(firstEntry.Current);
+                        ShowChannel(firstEntry.Current);
                     }
+
+                    //remove both from list and dropdown
+                    myChannels.Remove(channelName);
+                    ChannelDropdown.options.RemoveAll(item => item.text.Equals(channelName));
                 }
                 else//trying to unsuscribe to a channel not subscribed to.
                 {
                     Debug.Log("Can't unsubscribe from channel '" + channelName + "' because you are currently not subscribed to it.");
                 }
             }
+
         }
 
 
