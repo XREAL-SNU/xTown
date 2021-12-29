5e13c2711a69544993534041132e0194a167524b
Hyewon Bae <hyewon0309@snu.ac.kr>
2021-12-29 22:03:20 +0900

    Add Cinemachine
    Add Cinemachine
    Change Avatar size and walk/run speed

--- Assets/Scripts/LoadCharacter.cs
+++ Assets/Scripts/LoadCharacter.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class LoadCharacter : MonoBehaviour
 {
     public GameObject[] CharacterPrefabs;
     public Transform SpawnPoint;
+    public Transform FollowTarget;
+    public GameObject Player;
+    public CinemachineVirtualCamera vcam;
 
     // Start is called before the first frame update
     void Start()
     {
+        //vcam = GetComponent<CinemachineVirtualCamera>();
         int selectedCharacter = PlayerPrefs.GetInt("selectedCharacter");
         GameObject Prefab = CharacterPrefabs[selectedCharacter];
         GameObject Clone = Instantiate(Prefab, SpawnPoint.position, Quaternion.identity);
+        Player = null;
+    }
+
+    void Update()
+    {
+        if (Player == null)
+        {
+            Player = GameObject.FindWithTag("Player");
+            if (Player != null)
+            {
+                FollowTarget = Player.transform;
+                vcam.Follow = FollowTarget;
+                vcam.LookAt = FollowTarget;
+            }
+        }
     }
 }
