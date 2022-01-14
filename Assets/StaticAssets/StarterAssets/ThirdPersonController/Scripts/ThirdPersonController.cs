4f60d4b01f5a38f7f54dc45fdb4d351c7f0dae46
AliceOfSNU <khjune29@gmail.com>
2022-01-14 17:54:20 +0900

    [feature] - avatar controller auto decelerate - press R to enable mouse input for rotation - ESC to disable mouse input for view

--- Assets/StaticAssets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ Assets/StaticAssets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -10,48 +10,31 @@ namespace StarterAssets
 	public class ThirdPersonController : MonoBehaviour
 	{
 		[Header("Player")]
-		[Tooltip("Move speed of the character in m/s")]
 		public float MoveSpeed = 2.0f;
-		[Tooltip("Sprint speed of the character in m/s")]
 		public float SprintSpeed = 5.335f;
-		[Tooltip("How fast the character turns to face movement direction")]
 		[Range(0.0f, 0.3f)]
 		public float RotationSmoothTime = 0.12f;
-		[Tooltip("Acceleration and deceleration")]
 		public float SpeedChangeRate = 10.0f;
 
 		[Space(10)]
-		[Tooltip("The height the player can jump")]
 		public float JumpHeight = 1.2f;
-		[Tooltip("The character uses its own gravity value. The engine default is -9.81f")]
 		public float Gravity = -15.0f;
 
 		[Space(10)]
-		[Tooltip("Time required to pass before being able to jump again. Set to 0f to instantly jump again")]
 		public float JumpTimeout = 0.50f;
-		[Tooltip("Time required to pass before entering the fall state. Useful for walking down stairs")]
 		public float FallTimeout = 0.15f;
 
 		[Header("Player Grounded")]
-		[Tooltip("If the character is grounded or not. Not part of the CharacterController built in grounded check")]
 		public bool Grounded = true;
-		[Tooltip("Useful for rough ground")]
 		public float GroundedOffset = -0.14f;
-		[Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
 		public float GroundedRadius = 0.28f;
-		[Tooltip("What layers the character uses as ground")]
 		public LayerMask GroundLayers;
 
 		[Header("Cinemachine")]
-		[Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
 		public GameObject CinemachineCameraTarget;
-		[Tooltip("How far in degrees can you move the camera up")]
 		public float TopClamp = 70.0f;
-		[Tooltip("How far in degrees can you move the camera down")]
 		public float BottomClamp = -30.0f;
-		[Tooltip("Additional degress to override the camera. Useful for fine tuning camera position when locked")]
 		public float CameraAngleOverride = 0.0f;
-		[Tooltip("For locking the camera position on all axis")]
 		public bool LockCameraPosition = false;
 
 		// cinemachine
@@ -130,10 +113,12 @@ namespace StarterAssets
 
 		}
 
+		/*
 		private void LateUpdate()
 		{
 			CameraRotation();
 		}
+		*/
 
 		private void AssignAnimationIDs()
 		{
