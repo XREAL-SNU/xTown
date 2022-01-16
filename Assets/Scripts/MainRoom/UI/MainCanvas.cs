f6b0b479a713dd104e7041c488be88bbb1554229
ahnjinmo <ahnjinmo@snu.ac.kr>
2022-01-17 04:44:22 +0900

    [feat] added basic mainUI frame
    added basic mainUI frame

---
+++ Assets/Scripts/MainRoom/UI/MainCanvas.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCanvas : MonoBehaviour
+{
+    private MainCanvases _mainCanvases;
+
+    public void FirstInitialize(MainCanvases canvases)
+    {
+        _mainCanvases = canvases;
+    }
+
+    public void OnClick_PlayerInfo()
+    {
+        _mainCanvases.PlayerInfoCanvas.Show();
+    }
+
+    public void OnClick_Setting()
+    {
+        _mainCanvases.SettingCanvas.Show();
+    }
+
+    public void OnClick_Exit()
+    {
+        // Exit game
+    }
+
+    public void OnClick_AvatarInteraction()
+    {
+        _mainCanvases.AvatarInteractionCanvas.Show();
+    }
+}
