e1ef46dd96a2ee4b50d856d277a31d01a32e5a9c
tehokim <tehokim@snu.ac.kr>
2022-01-31 23:02:00 +0900

    0131
    아직 수정 중

---
+++ Assets/Scripts/Lobby/UI/AvatarCustomization/AvatarClass/CustomizablePart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizablePart
+{
+    public Color MyColor;
+    public Material MyMaterial;
+
+
+}
+
+public class CustomizableBackPack: CustomizablePart
+{
+
+}
+
