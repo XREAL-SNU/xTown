c5b97d5014b9948e6ae4ec508e74ffafa266c577
AliceOfSNU <khjune29@gmail.com>
2022-02-03 17:19:54 +0900

    -added speaker to character prefab -added recorder to mainroom -some UI scripting

---
+++ Assets/Scripts/AudioChat/AudioChatSettings.cs
@@ -0,0 +1,24 @@
+using Photon.Voice.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace XReal.XTown.VoiceChat
+{
+    public class AudioChatSettings : MonoBehaviour
+    {
+        Recorder _recorder;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _recorder = GetComponent<Recorder>();
+            var devices = _recorder.MicrophonesEnumerator;
+            foreach (var device in devices)
+            {
+                Debug.Log("device: " + device.IDString);
+            }
+            GetComponent<Recorder>().MicrophoneDevice = devices.GetEnumerator().Current;
+        }
+    }
+}
+
