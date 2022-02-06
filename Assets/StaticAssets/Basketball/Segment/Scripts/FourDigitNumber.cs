be5bcb0aab0a06517b91934fb073934f75dec4be
ahnjinmo <ahnjinmo@snu.ac.kr>
2022-02-07 08:51:43 +0900

    added 7 segment display
    added 7 segment display

---
+++ Assets/StaticAssets/Basketball/Segment/Scripts/FourDigitNumber.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using UnityEngine;
+using static System.Linq.Enumerable;
+
+public class FourDigitNumber : MonoBehaviour {
+    private int maxDigits = 4;
+    private SevenSegmentDisplay[] displays;
+
+    private void Awake() {
+        displays = GetComponentsInChildren<SevenSegmentDisplay>();
+        SetNumber(0);
+    }
+
+    public void SetNumber(int digits) {
+        foreach (var i in Range(0, displays.Length)) {
+            var display = displays[i];
+            if (i > 0 && digits == 0)
+                display.TurnOff();  // Suppress leading zeroes
+            else {
+                var rightmostDigit = digits % 10;
+                display.SetDigit(rightmostDigit);
+                digits /= 10;
+            }
+        }
+    }
+}
