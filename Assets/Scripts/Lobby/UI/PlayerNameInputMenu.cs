4924e185131bb061c33f6c3159d90f44527266e0
Adam <cmlee3211@gmail.com>
2022-02-07 17:07:30 +0900

    Fix Request Errors

--- Assets/Scripts/Lobby/UI/PlayerNameInputMenu.cs
+++ Assets/Scripts/Lobby/UI/PlayerNameInputMenu.cs
@@ -5,6 +5,8 @@ using UnityEngine.Networking;
 using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 
 public class PlayerNameInputMenu : MonoBehaviourPunCallbacks
@@ -39,18 +41,21 @@ public class PlayerNameInputMenu : MonoBehaviourPunCallbacks
         }
 
         List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
-        formData.Add(new MultipartFormDataSection("field1="+_playerName.text+"&field2="+_playerPassword.text));
+        formData.Add(new MultipartFormDataSection("name="+_playerName.text+"&pw="+_playerPassword.text));
         
         string url = "http://localhost:3000/enter";
         UnityWebRequest www = UnityWebRequest.Post(url, formData);
-        yield return www.SendWebRequest();
+        // yield return www.SendWebRequest();
+        SendRequest(www);
 
         if (www.result != UnityWebRequest.Result.Success)
         {
             Debug.LogError(www.error);
             return;
         }
-        Debug.Log("Form upload complete!");
+        Debug.Log("Complete!");
+        // JObject json = JObject.Parse(www.result);
+        Debug.Log("Result:" + "\n" + www.result);
 
         PhotonNetwork.NickName = _playerName.text;
         Debug.Log("Player Name is "+ _playerName.text, this);
@@ -59,6 +64,10 @@ public class PlayerNameInputMenu : MonoBehaviourPunCallbacks
 
         _roomCanvases.AvatarSelectionCanvas.Show();
         _roomCanvases.PlayerNameInputCanvas.Hide();
+    }
+
+    IEnumerator SendRequest(UnityWebRequest www) {
+        yield return www.SendWebRequest();
     }
 /*
     public void OnClick_JoinLobby()
