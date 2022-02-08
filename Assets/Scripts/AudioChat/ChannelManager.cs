6be05074909c310d880620272bd860728ba8d0ad
AliceOfSNU <khjune29@gmail.com>
2022-02-08 12:58:51 +0900

    [feat] List of players in voice room

---
+++ Assets/Scripts/AudioChat/ChannelManager.cs
@@ -0,0 +1,17 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChannelManager : MonoBehaviour
+{
+    static List<>
+    [PunRPC]
+    void AddUser()
+    {
+
+    }
+
+    PhotonView view;
+    view.RPC()
+}
