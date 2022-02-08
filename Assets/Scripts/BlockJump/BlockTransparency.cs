fd746b5b5bca437dd79163a859d251cba21267ba
westkite2 <81609751+westkite2@users.noreply.github.com>
2022-02-08 23:48:06 +0900

    week2 combined
    commit before pushing to dev

---
+++ Assets/Scripts/BlockJump/BlockTransparency.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTransparency : MonoBehaviour
+{
+    public float fadeSpeed;
+    private bool isPlayerOnBlock = false;
+
+    Material Mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mat = GetComponent<Renderer>().material;
+        if(fadeSpeed == 0)
+        {
+            fadeSpeed = 0.01f;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        isPlayerOnBlock = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isPlayerOnBlock = false;
+        StopAllCoroutines();
+        StartCoroutine(FadeInObject());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isPlayerOnBlock == true)
+        {
+            StartCoroutine(FadeOutObject());
+        }
+    }
+
+    IEnumerator FadeOutObject()
+    {
+        while (Mat.color.a > 0.01f)
+        {
+            Color objectColor = Mat.color;
+            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            Mat.color = objectColor;
+            yield return null;
+        }
+        if (Mat.color.a <= 0.01f)
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+    IEnumerator FadeInObject()
+    {
+        gameObject.GetComponent<BoxCollider>().enabled = true;
+        while (Mat.color.a <= 0.5f)
+        {
+            Color objectColor = Mat.color;
+            float fadeAmount = objectColor.a + (Time.deltaTime);
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            Mat.color = objectColor;
+            yield return null;
+        }
+    }
+}
