3b96ce37ad8387535875ac463e2cf7b8a5fbcaff
westkite2 <81609751+westkite2@users.noreply.github.com>
2022-02-09 15:17:41 +0900

    add comments on scripts
    add comments on scripts

--- Assets/Scripts/BlockJump/BlockTransparency.cs
+++ Assets/Scripts/BlockJump/BlockTransparency.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class BlockTransparency : MonoBehaviour
 {
-    public float fadeSpeed;
-    private bool isPlayerOnBlock = false;
+    /* 플레이어가 블록에 접촉 시, 블록이 투명해지게 합니다. */
+    /* 플레이어가 접촉하지 않으면 다시 불투명하게 돌아옵니다. */
 
     Material Mat;
+    public float fadeSpeed; //블록 투명화 속도
+    private bool isPlayerOnBlock = false; //플레이어의 블록 접촉 여부
 
-    // Start is called before the first frame update
     void Start()
     {
         Mat = GetComponent<Renderer>().material;
-        if(fadeSpeed == 0)
+        if (fadeSpeed == 0)
         {
-            fadeSpeed = 0.01f;
+            fadeSpeed = 0.01f; //투명화 속도 기본값
         }
     }
     private void OnTriggerEnter(Collider other)
@@ -24,45 +25,55 @@ public class BlockTransparency : MonoBehaviour
     }
     private void OnTriggerExit(Collider other)
     {
-        isPlayerOnBlock = false;
-        StopAllCoroutines();
-        StartCoroutine(FadeInObject());
+        isPlayerOnBlock = false; //플레이어가 블록에 접촉 중이 아닌 경우,
+        StopAllCoroutines(); //투명화를 중단하고,
+        StartCoroutine(FadeInObject()); //다시 불투명화
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
         if (isPlayerOnBlock == true)
         {
+            //플레이어가 블록 접촉 시, 블록 투명화
             StartCoroutine(FadeOutObject());
         }
     }
 
     IEnumerator FadeOutObject()
     {
-        while (Mat.color.a > 0.01f)
+        /* 블록 투명화하는 함수 */
+        while (Mat.color.a > 0.01f) //블록이 불투명한 경우
         {
-            Color objectColor = Mat.color;
-            float fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
-            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
-            Mat.color = objectColor;
+            //블록 알파값 감소
+            Color ObjectColor = Mat.color;
+            float fadeAmount = ObjectColor.a - (fadeSpeed * Time.deltaTime);
+
+            //감소된 알파값으로 블록 색 재설정
+            ObjectColor = new Color(ObjectColor.r, ObjectColor.g, ObjectColor.b, fadeAmount);
+            Mat.color = ObjectColor;
+
             yield return null;
         }
-        if (Mat.color.a <= 0.01f)
+        if (Mat.color.a <= 0.01f) //블록이 투명해진 경우
         {
+            //블록 콜라이더를 비활성화하여 플레이어가 추락하게 함
             gameObject.GetComponent<BoxCollider>().enabled = false;
         }
     }
 
     IEnumerator FadeInObject()
     {
+        /* 블록이 다시 불투명해지게 하는 함수 */
         gameObject.GetComponent<BoxCollider>().enabled = true;
-        while (Mat.color.a <= 0.5f)
+        while (Mat.color.a <= 0.5f) //초기 불투명도로 돌아오기 전까지
         {
+            // 블록 알파값 증가
             Color objectColor = Mat.color;
             float fadeAmount = objectColor.a + (Time.deltaTime);
+            //증가된 알파값으로 블록 색 재설정
             objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
             Mat.color = objectColor;
+
             yield return null;
         }
     }
--- Assets/Scripts/BlockJump/CoinScript.cs
+++ Assets/Scripts/BlockJump/CoinScript.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinScript : MonoBehaviour
-{    
+{
+    /* 코인이 회전하도록 합니다. */
+    /* 플레이어가 획득하면 사라지도록 합니다. */
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        //Coin rotation
-        transform.Rotate(new Vector3(200f, 0f, 0f) * Time.deltaTime);
+        transform.Rotate(new Vector3(200f, 0f, 0f) * Time.deltaTime); //코인이 회전하도록 함
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        gameObject.SetActive(false);
-
+        gameObject.SetActive(false); //플레이어가 접촉(획득) 시 사라짐
     }
 
 }
