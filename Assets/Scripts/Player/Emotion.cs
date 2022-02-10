2e63acfac547747200ac87bc69a56a0cee4e3170
tnsqjahong <52613351+tnsqjahong@users.noreply.github.com>
2022-02-10 20:35:22 +0900

    [FEAT] 1인칭일때 얼굴비추는 카메라

--- Assets/Scripts/Player/Emotion.cs
+++ Assets/Scripts/Player/Emotion.cs
@@ -11,14 +11,19 @@ public class Emotion : MonoBehaviour
     public RectTransform EmoticonMenu;
     [Header("MenuUI")]
     public Image[] MenuSlice;
+    public GameObject Face;
     private int _currentMenu=0;
     private bool _isMenuActive = false;
-    public GameObject Face;
     private List<AvatarFaceButton> _faceList;
-    private PhotonView _view;
     private bool _isSelected;
     private AvatarFaceControl _avatarFaceControl;
+    private GameObject _camManager;
     private CameraControl _characterCam;
+    private bool _isCurrentFp;
+    public Transform _faceCam;
+    private bool _crRunning;
+    private IEnumerator _coroutine;
+    private PhotonView _view;
 
     private void Start()
     {
@@ -26,32 +31,43 @@ public class Emotion : MonoBehaviour
         Player = GameObject.FindWithTag("Player").transform;
         _avatarFaceControl = Player.GetChild(2).GetChild(0).GetChild(1).GetComponent<AvatarFaceControl>();
         _view = GetComponent<PhotonView>();
+        _camManager = GameObject.Find("CamManager");
         _characterCam = GameObject.Find("CharacterCam").GetComponent<CameraControl>();
+        _faceCam = GameObject.Find("MainCanvases").transform.GetChild(4).GetChild(1);
     }
 
     private void Update()
     {
-        Debug.Log(_avatarFaceControl);
-        Debug.Log(_characterCam);
         // if (_view is null || !_view.IsMine) return;
         if (Input.GetKeyDown(KeyCode.T))
         {
-            _characterCam.SetFront();
             _faceList = Face.GetComponent<AvatarFaceManagement>()._favList;
             for(int i = 0;i<4;i++){
                 MenuSlice[i].transform.GetChild(1).GetComponent<Text>().text = _faceList[i].GetButtonText().text;
             }
             if(!_isMenuActive){
+                if(_camManager.GetComponent<CamManager>().IsCurrentFp){
+                    _faceCam.gameObject.SetActive(true);
+                } else {
+                    _characterCam.SetFront();
+                }
                 EmoticonMenu.gameObject.SetActive(true);
                 _isMenuActive = true;
             }else{
+                if(_camManager.GetComponent<CamManager>().IsCurrentFp){
+                    _faceCam.gameObject.SetActive(false);
+                }
                 EmoticonMenu.gameObject.SetActive(false);
                 _isMenuActive = false;
             }
         }
         if(_isMenuActive){
-            Vector3 screenPos = Camera.main.WorldToScreenPoint(new Vector3(Player.position.x,Player.position.y+2f,Player.position.z));
-            EmoticonMenu.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(screenPos.x,screenPos.y,Player.position.z);
+            if(!_camManager.GetComponent<CamManager>().IsCurrentFp){
+                Vector3 screenPos = Camera.main.WorldToScreenPoint(new Vector3(Player.position.x,Player.position.y+2f,Player.position.z));
+                EmoticonMenu.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(screenPos.x,screenPos.y,Player.position.z);
+            }else{
+                EmoticonMenu.gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(1400,400);
+            }
             
             if(Input.GetKeyDown(KeyCode.Alpha1)){
                 MenuSlice[0].color = Color.black;
@@ -74,13 +90,19 @@ public class Emotion : MonoBehaviour
                 for(int i=0;i<4;i++){
                     MenuSlice[i].color = Color.white;
                 }
-                _characterCam.SetFront();
                 EmoticonSelect(_currentMenu);
             }
         }
+        // if(Input.GetKeyDown(KeyCode.G)){
+        //     _faceCam.gameObject.SetActive(!_faceCam.gameObject.activeSelf);
+        // }
     }
     public void EmoticonSelect(int num){
-        StartCoroutine(ChangeFace(_faceList[num].GetImageIndex()));
+        if(_crRunning){
+            StopCoroutine(_coroutine);
+        }
+        _coroutine = ChangeFace(_faceList[num].GetImageIndex());
+        StartCoroutine(_coroutine);
         EmoticonMenu.gameObject.SetActive(false);
         _isMenuActive = false;
         _isSelected = false;
@@ -98,8 +120,11 @@ public class Emotion : MonoBehaviour
 
     IEnumerator ChangeFace(int faceIndex)
     {
+        _crRunning = true;
         _avatarFaceControl.ChangeFace(faceIndex);
         yield return new WaitForSeconds(10f);
         _avatarFaceControl.ChangeFace(11);
+        _faceCam.gameObject.SetActive(false);
+        _crRunning = false;
     }
 } 
