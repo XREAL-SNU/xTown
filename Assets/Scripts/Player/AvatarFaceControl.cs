b9de668e4bfccd48bd025afb5b7fe5eef1f90784
be9904 <be9904@gmail.com>
2022-02-12 17:41:38 +0900

    [feat] Change Avatar Face with Keys 1234

--- Assets/Scripts/Player/AvatarFaceControl.cs
+++ Assets/Scripts/Player/AvatarFaceControl.cs
@@ -8,9 +8,38 @@ public class AvatarFaceControl : MonoBehaviour
 {
     public Material AvatarFace;
 
-    [SerializeField] List<Texture> _emojiTextures;
+    [SerializeField] Texture _defaultTexture;
+    
+    bool _isDefault = true;
 
-    private void Start() { AvatarFace.SetTexture("_MainTex", _emojiTextures[11]); }
+    private void Start() { AvatarFace.SetTexture("_MainTex", _defaultTexture); }
 
-    public void ChangeFace(int faceIndex) { AvatarFace.SetTexture("_MainTex", _emojiTextures[faceIndex]); }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) StartCoroutine(ShowFaceForSeconds(AvatarFaceManagement.s_favList[0].GetImageIndex()));
+        if (Input.GetKeyDown(KeyCode.Alpha2)) StartCoroutine(ShowFaceForSeconds(AvatarFaceManagement.s_favList[1].GetImageIndex()));
+        if (Input.GetKeyDown(KeyCode.Alpha3)) StartCoroutine(ShowFaceForSeconds(AvatarFaceManagement.s_favList[2].GetImageIndex()));
+        if (Input.GetKeyDown(KeyCode.Alpha4)) StartCoroutine(ShowFaceForSeconds(AvatarFaceManagement.s_favList[3].GetImageIndex()));
+    }
+
+    public void ChangeFace(int faceIndex) 
+    {
+        AvatarFace.SetTexture("_MainTex", AvatarFaceManagement.s_avatarTextureList[faceIndex]);
+
+        if (AvatarFaceManagement.s_avatarTextureList[faceIndex].name.Equals("happy"))
+            _isDefault = true;
+        else
+            _isDefault = false;
+    }
+
+    IEnumerator ShowFaceForSeconds(int index)
+    {
+        if (!_isDefault) yield break;
+
+        ChangeFace(index);
+
+        yield return new WaitForSeconds(10f);
+
+        ChangeFace(AvatarFaceManagement.DefaultIndex);
+    }
 }
