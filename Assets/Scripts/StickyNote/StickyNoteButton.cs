35f6df75e95a0aafa7ada8d7f6e4d4bc2917e504
ahnjinmo <89832796+ahnjinmo@users.noreply.github.com>
2022-02-13 22:31:13 +0900

    [fix] fixed stickynote instantiate angle
    [fix] fixed stickynote instantiate angle

--- Assets/Scripts/StickyNote/StickyNoteButton.cs
+++ Assets/Scripts/StickyNote/StickyNoteButton.cs
@@ -13,9 +13,10 @@ public class StickyNoteButton : MonoBehaviour
     }
     public void CreateStickyNote()
     {
-        
+        Vector3 mainCamRotation = Camera.main.transform.rotation.eulerAngles;
+        Vector3 forwardRotation = new Vector3(0, mainCamRotation.y, 0);
         //if(StickyNoteNetworkManager.Instance.networked)
-            PhotonNetwork.Instantiate(stickyNotePrefab.name, _player.transform.position, Quaternion.identity);
+            PhotonNetwork.Instantiate(stickyNotePrefab.name, _player.transform.position, Quaternion.Euler(forwardRotation));
         //else
         //    Instantiate(stickyNotePrefab, Vector3.zero, Quaternion.identity);
     }
