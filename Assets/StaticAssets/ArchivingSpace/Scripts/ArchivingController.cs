61c37d7cbbc96aaaa8a8711d935a678b5f20284c
ahnjinmo <89832796+ahnjinmo@users.noreply.github.com>
2022-02-15 02:05:43 +0900

    added article images
    added article images

--- Assets/StaticAssets/ArchivingSpace/Scripts/ArchivingController.cs
+++ Assets/StaticAssets/ArchivingSpace/Scripts/ArchivingController.cs
@@ -18,26 +18,39 @@ public class ArchivingController : MonoBehaviour
 
     private RectTransform _archivingRT;
 
-    private Sequence _openSequence;
-    private Sequence _closeSequence;
-
     private void Start()
     {
         _archivingRT = _archivingCanvas.GetComponent<RectTransform>();
+    }
+    public void OnTriggerChange()
+    {
+        if (_isTriggered)
+        {
+            PlayOpenSequence();
+        }
+        else
+        {
+            PlayCloseSequence();
+        }
+    }
 
-        _openSequence = DOTween.Sequence().
-            OnStart(() =>
+    private void PlayOpenSequence()
+    {
+        Sequence _openSequence = DOTween.Sequence()
+            .OnStart(() =>
             {
                 Debug.Log("open");
                 _archivingCamera.SetActive(true);
                 _archivingCanvas.SetActive(true);
                 _archivingRT.sizeDelta = new Vector2(0, _archivingRT.sizeDelta.y);
             })
-            .Append(DOVirtual.Float(0, _canvasWidth, 1f, RectTransformSize)).SetEase(Ease.OutCubic)
-            .SetAutoKill(false);
+            .Append(DOVirtual.Float(0, _canvasWidth, 1f, RectTransformSize)).SetEase(Ease.OutCubic);
+    }
 
-        _closeSequence = DOTween.Sequence().
-            OnStart(() =>
+    private void PlayCloseSequence()
+    {
+        Sequence _closeSequence = DOTween.Sequence()
+            .OnStart(() =>
             {
                 Debug.Log("close");
                 _archivingCamera.SetActive(false);
@@ -46,33 +59,15 @@ public class ArchivingController : MonoBehaviour
             .Append(DOVirtual.Float(_canvasWidth, 0, 1f, RectTransformSize)).SetEase(Ease.OutCubic)
             .OnComplete(() =>
             {
-                _archivingCanvas.SetActive(false);
-            })
-            .SetAutoKill(false);
-
-        _openSequence.Pause();
-        _closeSequence.Pause();
-    }
-    public void OnTriggerChange()
-    {
-        if (_isTriggered)
-        {
-            _openSequence.Restart();
-        }
-        else
-        {
-            _closeSequence.Restart();
-        }
+                if (!_isTriggered)
+                {
+                    _archivingCanvas.SetActive(false);
+                }
+            });
     }
 
     private void RectTransformSize(float x)
     {
         _archivingRT.sizeDelta = new Vector2(x, _archivingRT.sizeDelta.y);
     }
-
-    private void PlaySequence()
-    {
-
-    }
-
 }
