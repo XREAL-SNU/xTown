2d3f6434b5ddd9f6f9ef813d13b74feceac91e4b
Yoon Jihee <74297999+yjh9929@users.noreply.github.com>
2022-02-15 04:27:58 +0900

    2022.02.15

---
+++ Assets/Scripts/BlockJump/Timer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text text_timer;
+    private float time;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        text_timer.text = time.ToString ();
+    }
+}
