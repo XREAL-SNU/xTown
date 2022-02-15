d3c0a4d6bf7ec1cf4ba808216ea311cf2ded3987
Yoon Jihee <74297999+yjh9929@users.noreply.github.com>
2022-02-15 12:56:59 +0900

    2022.02.15. temp

--- Assets/Scripts/BlockJump/GameManager.cs
+++ Assets/Scripts/BlockJump/GameManager.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // 게임 규칙 : 득점 현황
 
     static public Vector3 savePoint; //추락시 돌아오는 지점
+    public Text pointText;
+
     
     // Start is called before the first frame update
     void Start()
@@ -27,5 +29,10 @@ public class GameManager : MonoBehaviour
             other.transform.position = savePoint; //추락 시 savePoint로 이동
             other.GetComponent<Rigidbody>().velocity = Vector3.zero; //이동한 위치에서 정지
         }
+    }
+
+    private void EndGame()
+    {
+        pointText.text = pointText.ToString();
     }
 }
