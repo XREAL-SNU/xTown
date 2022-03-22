a1ab45d66d9484ef865222dee94e8b6f980d0cd4
be9904 <be9904@gmail.com>
2022-03-22 15:16:22 +0900

    Added QuickSlotManager

---
+++ Assets/Senior A/Junghoon/Scripts/QuickSlotManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuickSlotManager : MonoBehaviour
+{
+    [SerializeField] List<Sprite> _buttonIcons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
