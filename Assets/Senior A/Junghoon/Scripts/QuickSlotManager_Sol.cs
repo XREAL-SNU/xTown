f4f9cfe36720f6779da7073e707ec05c9e20f698
be9904 <be9904@gmail.com>
2022-03-24 15:05:37 +0900

    Change UI Buttons to Instantiate on Start()

--- Assets/Senior A/Junghoon/Scripts/QuickSlotManager_Sol.cs
+++ Assets/Senior A/Junghoon/Scripts/QuickSlotManager_Sol.cs
@@ -5,33 +5,45 @@ using UnityEngine.EventSystems;
 
 public class QuickSlotManager_Sol : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField] List<Sprite> _buttonIcons;
+    // Reference(s) in Scene
     [SerializeField] List<QuickSlotButton_Sol> _quickSlots;
-
+    [SerializeField] List<Sprite> _buttonIcons;
     public Transform GridLayout;
+
+    // Reference(s) in Directory    
+    [SerializeField] GameObject _viewportButtonPrefab;
 
     public static QuickSlotButton_Sol CurrentlySelected;
     static List<QuickSlotButton_Sol> s_quickSlots;
 
     void Start()
     {
+        GameObject buttonInstance;
         QuickSlotButton_Sol buttonProp;
 
-        for(int i = 0; i < GridLayout.childCount; i++)
+        for(int i = 0; i < _buttonIcons.Count; i++)
         {
-            buttonProp = GridLayout.GetChild(i).GetComponent<QuickSlotButton_Sol>();
+            // Instantiate at Start of Game
+            buttonInstance = Instantiate(_viewportButtonPrefab, GridLayout);
+            buttonProp = buttonInstance.GetComponent<QuickSlotButton_Sol>();
+
+            // Set QuickSlotButton Properties
             buttonProp.ButtonImage.sprite = _buttonIcons[i];
             buttonProp.ButtonText.text = _buttonIcons[i].name;
             buttonProp.fid = i;
         }
 
+        // Allocate Memory for s_quickSlots
         s_quickSlots = new List<QuickSlotButton_Sol>();
+
+        // Add References of Quick Slot Buttons to Static List
         for (int i = 0; i < _quickSlots.Count; i++)
         {
             s_quickSlots.Add(_quickSlots[i]);
         }
     }
 
+    // Function to Add Button to Quick Slots
     public static void AddToQuickSlot(QuickSlotButton_Sol quickslotButton)
     {
         int index = CheckDistinct();
@@ -50,6 +62,7 @@ public class QuickSlotManager_Sol : MonoBehaviour, IPointerClickHandler
         CurrentlySelected = null;
     }
 
+    // Check if Button is already added
     static int CheckDistinct()
     {
         int retVal = -1;
@@ -63,6 +76,7 @@ public class QuickSlotManager_Sol : MonoBehaviour, IPointerClickHandler
         return retVal;
     }
 
+    // Change Location of Buttons. Should only be called if button type is QuickSlot
     static void SwapButtons(QuickSlotButton_Sol quickslotButton, int index)
     {
         Sprite spriteBuf = quickslotButton.ButtonImage.sprite;
@@ -78,6 +92,7 @@ public class QuickSlotManager_Sol : MonoBehaviour, IPointerClickHandler
         s_quickSlots[index].fid = idBuf;
     }
 
+    // Clear Buffer if Canvas is Clicked
     public void OnPointerClick(PointerEventData eventData)
     {
         CurrentlySelected = null;
