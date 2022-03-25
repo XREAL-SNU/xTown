93db39234aa1fc93af998127411c8b4ecb831b27
Hyewon Bae <hyewon0309@snu.ac.kr>
2022-03-25 19:28:47 +0900

    Revert "Merge branch 'Senior_A_Base' of https://github.com/XREAL-SNU/xTown into Senior_A_Base"
    This reverts commit 9c4bdb743730d1001ba32cbbe0c4b55694c67829, reversing
    changes made to 633d4f26304181647dc8ad3978362bf65fd0b5fb.

--- Assets/Senior A/Soonbeom/CamManager.cs
+++ Assets/Senior A/Soonbeom/CamManager.cs
@@ -1,23 +1,19 @@
 /*
-카메라 과제
-----------------------------------------------------
+카메라 과제:
 본 과제에서는 카메라의 1인칭과 3인칭 변환을 구현해보도록 하겠습니다.
+3인칭시점에서는 마우스 우클릭 후 드래그 시 시점을 조절할 수 있도록 합니다. 그리고 마우스 스크롤을 통해 줌을 할 수 있게 합니다.
+단, 줌을 할 경우 너무 가깝거나 너무 먼 거리에서 찍지 못하게 적절한 수준 사이에서만 허용합니다.
+그리고 카메라를 내렸을 때 아래에서 바라봐 하늘을 볼 수 있는 정도까지 시점변환을 허용합니다.
+카메라가 벽을 뚫고 들어가서 플레이어를 찍을 수 없다거나, 중간에 장애물이 가로막아서 플레이어를 찍지 못 하는 불편한 점이 없도록 합니다.
+단 땅과 카메라가 충돌했을 때에는 하늘을 볼 수 있어야 하므로 땅을 무시하고 아래에서 찍을 수 있도록 합니다.
+1인칭시점에서는 플레이어의 시선에서 씬을 바라보도록 합니다. 플레이어가 회전 시 카메라가 보는 화면도 회전해야하며 플레이어 시선과 같은 높이에서 씬을 바라보게 합니다.
+3인칭과 1인칭 시점 전환시 1초 동안 부드럽게 화면을 전환합니다.
 
-1. 1인칭과 3인칭 시점을 찍을 적절한 가상카메라를 추가하기
-2. 3인칭 카메라 시점
-    - 적절한 거리에서 플레이어를 찍을 것
-    - 마우스 우클릭 후 드래그로 시점 조절이 가능하게 할 것
-    - 카메라 회전 시 하늘을 볼 수 있을 정도로 회전 가능하게 할 것
-    - 마우스 스크롤으로 줌을 할 수 있게 할 것.
-    - 카메라가 플레이어를 찍으면서 벽과 부딪히거나 장애물에 가리는 일이 없도록 할 것. 단 바닥과 충돌 시에는 무시한다.
-    - FXAA모드로 안티앨리어싱을 적용하고, 임의의 포스트 프로세싱 하나 이상 추가하기.
-3. 1인칭 카메라 시점
-    - 플레이어의 눈높이에서 바라보는 세상을 보여줄 것
-    - 플레이어의 얼굴을 보여주는 창을 오른쪽 아래에 띄웁니다.
-4. 카메라 제어는 camManager를 통해 합니다.
-5. 1인칭과 3인칭 카메라 전환시 1초의 시간동안 부드럽게 전환합니다.
+과제에 대한 평가기준은 다음과 같습니다.
+1. 1인칭 시점과 3인칭 시점에 적절한 가상카메라를 사용할 것
+2. CamManager오브젝트와 CamManager 스크립트를 통해 카메라 관리를 할 것
+3. 3인칭 시점일 시 플레이어를 바라보는 데 불편함이 없을 것
 
----------------------------------------------------
 과제에 대한 구현은 강의자료를 참고하시기 바랍니다.
 */
 
@@ -30,12 +26,9 @@ public class CamManager : MonoBehaviour
 {
     public GameObject thirdPersonCamObj;
     public GameObject firstPersonCamObj;
-    public GameObject faceCamCanvas;
-    public GameObject faceCam;
     public CinemachineFreeLook thirdPersonCam;
     public CinemachineVirtualCamera firstPersonCam;
     public GameObject player;
-    public GameObject playerFace;
     public float xRotateSpeed = 500.0f;
     public float yRotateSpeed = 10.0f;
     public float zoomSpeed = 0.5f;
@@ -44,8 +37,7 @@ public class CamManager : MonoBehaviour
 
     private void Start() {
         // 카메라가 쫓아다닐 플레이어를 설정합니다.
-        player = GameObject.FindWithTag("Player").gameObject;
-        playerFace = player.transform.Find("FollowTarget").gameObject;
+        player = GameObject.FindWithTag("Player");
 
         // 1인칭 카메라와 3인칭카메라를 초기화시키고 카메라의 타겟을 설정합니다.
         firstPersonCamObj = player.transform.Find("FirstPersonCam").gameObject;
@@ -54,18 +46,18 @@ public class CamManager : MonoBehaviour
         thirdPersonCam = thirdPersonCamObj.GetComponent<CinemachineFreeLook>();
         thirdPersonCam.Follow = player.transform;
         thirdPersonCam.LookAt = player.transform;
-        faceCamCanvas = GameObject.Find("FaceCamCanvas");
-        faceCam = faceCamCanvas.transform.Find("FaceCam").gameObject;
 
         // 처음에는 3인칭카메라로 시작할 것이기 때문에 1인칭카메라를 비활성화시켜줍니다.
         firstPersonCam.gameObject.SetActive(false);
         thirdPersonCam.gameObject.SetActive(true);
-        faceCamCanvas.gameObject.SetActive(false);
         _isCurrentFp = false;
     }
     private void Update(){
         // 키보드 G 키를 누르면 1인칭과 3인칭을 전환합니다.
         if(Input.GetKeyDown(KeyCode.G)){
+            if(_isCurrentFp){
+                _isCurrentFp = !_isCurrentFp;
+            }
             CamChange();
         }
         // 마우스를 클릭했을때 3인칭 시점변경을 활성화시킵니다.
@@ -84,26 +76,19 @@ public class CamManager : MonoBehaviour
             thirdPersonCam.m_XAxis.m_MaxSpeed = 0;
             thirdPersonCam.m_YAxis.m_MaxSpeed = 0;
         }
-        if(_isCurrentFp){
-            faceCam.transform.position =  playerFace.transform.position + player.transform.forward * 0.9f;
-            faceCam.transform.LookAt(playerFace.transform.position);
-        }
         if(Input.mouseScrollDelta.y != 0 && !_isCurrentFp){
             Zoom();
         }
 	}
     // CamChange에서 1인칭과 3인칭카메라를 변환합니다.
     private void CamChange(){
-        _isCurrentFp = !_isCurrentFp;
         if(thirdPersonCam.gameObject.activeSelf){
             firstPersonCam.gameObject.SetActive(true);
             thirdPersonCam.gameObject.SetActive(false);
-            faceCamCanvas.gameObject.SetActive(true);
         }
         else{
             firstPersonCam.gameObject.SetActive(false);
             thirdPersonCam.gameObject.SetActive(true);
-            faceCamCanvas.gameObject.SetActive(false);
         }
     }
     // RotateTp에서 3인칭 시점 조절을 합니다.
